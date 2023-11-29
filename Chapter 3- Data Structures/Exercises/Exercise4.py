3314be69b743ff5b2e84b8ee3ddd9d32576ef118
NizamShareefajm <152371854+NizamShareefajm@users.noreply.github.com>
2023-11-29 22:57:25 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,7 @@
+guests = ['Nikola Tesla', 'Maya Angelou', 'Albert Einstein']
+
+for guest in guests:
+    print(f"Dear {guest},")
+    print("I would like to invite you for dinner.")
+    print("Please let me know if you would be available. Dinner will be a delightful occasion!")
+    print("\n")
